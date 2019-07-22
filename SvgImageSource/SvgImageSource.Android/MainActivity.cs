1afd143b4a3cec6f85a0fc707c9694b7007c4878
candidodmv <candidodmv@gmail.com>
2019-07-22 19:21:00 -0300

    install and configure FFImageLoading

--- SvgImageSource/SvgImageSource.Android/MainActivity.cs
+++ SvgImageSource/SvgImageSource.Android/MainActivity.cs
@@ -21,6 +21,9 @@ namespace SvgImageSource.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(false);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
