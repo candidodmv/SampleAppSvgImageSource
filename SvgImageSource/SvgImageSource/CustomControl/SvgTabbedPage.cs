2fd40146e86601ff5e54bd4b70898f3b4f904833
candidodmv <candidodmv@gmail.com>
2019-07-23 00:54:07 -0300

    custom renderers

---
+++ SvgImageSource/SvgImageSource/CustomControl/SvgTabbedPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SvgImageSource.CustomControl
+{
+    public class SvgTabbedPage : TabbedPage
+    {
+    }
+}
--- SvgImageSource/SvgImageSource/TabbedPage1.xaml.cs
+++ SvgImageSource/SvgImageSource/TabbedPage1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SvgImageSource.CustomControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ using Xamarin.Forms.Xaml;
 namespace SvgImageSource
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TabbedPage1 : TabbedPage
+    public partial class TabbedPage1 : SvgTabbedPage
     {
         public TabbedPage1()
         {
